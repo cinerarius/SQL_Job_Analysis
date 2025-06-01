4a6ebde46250281ab72d4f45a5db8fb061932873
cinerarius <6cine6rarius6@gmail.com>
2025-06-02 03:33:07 +0530

    .1

--- project_sql/4_top_skills_to_role.sql
+++ project_sql/4_top_skills_to_role.sql
@@ -1,26 +1,13 @@
-WITH job_posting AS
-    (SELECT
-        job_id,
-        job_title,
-        job_location,
-        job_schedule_type,
-        salary_year_avg,
-        job_posted_date
-    FROM job_postings_fact
-    WHERE 
-        job_title_short = 'Data Analyst' AND
-        salary_year_avg IS NOT NULL 
-    ORDER BY
-        salary_year_avg DESC)
-
-SELECT 
+SELECT
     skills_dim.skills,
     ROUND(AVG(salary_year_avg), 2) AS avg_salary
-FROM job_posting
+FROM job_postings_fact
 INNER JOIN skills_job_dim
-    ON job_posting.job_id = skills_job_dim.job_id
+ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim
-    ON skills_job_dim.skill_id = skills_dim.skill_id
+ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+    job_title_short = 'Data Analyst' AND
+    salary_year_avg IS NOT NULL 
 GROUP BY skills_dim.skills
-ORDER BY avg_salary DESC
 LIMIT 30;
--- project_sql/5_optimal_skills.sql
+++ project_sql/5_optimal_skills.sql
@@ -1,6 +1,7 @@
 WITH skills_demand AS
     (SELECT
         skills_dim.skill_id,
+        skills_dim.skills AS skills,
         COUNT(job_postings_fact.job_id) AS job_count
     FROM job_postings_fact
     INNER JOIN skills_job_dim
@@ -9,12 +10,14 @@ WITH skills_demand AS
         ON skills_job_dim.skill_id = skills_dim.skill_id
     WHERE 
         job_title = 'Data Analyst' AND
-        salary_year_avg IS NOT NULL 
+        salary_year_avg IS NOT NULL AND
+        job_work_from_home = TRUE
     GROUP BY skills_dim.skill_id
 ),
 average_salary AS
     (SELECT
         skills_dim.skill_id,
+        skills_dim.skills,
         ROUND(AVG(salary_year_avg), 2) AS avg_salary
     FROM job_postings_fact
     INNER JOIN skills_job_dim
@@ -23,15 +26,19 @@ average_salary AS
     ON skills_job_dim.skill_id = skills_dim.skill_id
     WHERE 
         job_title_short = 'Data Analyst' AND
-        salary_year_avg IS NOT NULL 
+        salary_year_avg IS NOT NULL AND
+        job_work_from_home = TRUE
     GROUP BY skills_dim.skill_id
 )
 SELECT 
-    skills_demand.skill_id,
-    skills_dim.skills,
-    skills_demand.job_count,
-    average_salary.avg_salary
+    skills_demand.skills,
+    job_count,
+    avg_salary
 FROM
     skills_demand
     INNER JOIN average_salary ON average_salary.skill_id = skills_demand.skill_id
-    INNER JOIN skills_dim ON skills_demand.skill_id = skills_dim.skill_id;
+WHERE 
+    job_count > 5
+ORDER BY 
+    avg_salary DESC,
+    job_count DESC
